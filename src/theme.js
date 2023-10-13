a7e4950d549357e3b89786d1d43e2bf7eff144bb
ohhoho8 <haeryung8@gmail.com>
2023-10-13 15:51:43 +0900

    second commit: preparing projects

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+  white: "#ffffff",
+  black: "#000000",
+  grey_0: "#d5d5d5",
+  grey_1: "#a6a6a6",
+  red: "#e84118",
+  blue: "#3679fe",
+};
+
+export const theme = {
+  background: colors.white,
+  text: colors.black,
+};
